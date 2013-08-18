a3285d02ebb7c614f104a218e267596af9538fa4
cioddi <maxtobiasweber@gmail.com>
2013-08-18 16:47:24 +0200

    first tests

--- tests/before_script.sh
+++ tests/before_script.sh
@@ -19,7 +19,7 @@ echo "Starting Selenium"
 if [ ! -f $serverFile ]; then
     wget http://selenium.googlecode.com/files/$serverFile
 fi
-xvfb-run java -jar $serverFile > /tmp/selenium.log &
+sudo xvfb-run java -jar $serverFile > /tmp/selenium.log &
 
 wget --retry-connrefused --tries=60 --waitretry=1 --output-file=/dev/null $serverUrl/wd/hub/status -O /dev/null
 if [ ! $? -eq 0 ]; then
